ae21b9c5c1c7a77b5e6392557ab052f710960b78
Yan Mingzhi <yanmingzhi@elastos.org>
2019-02-10 20:17:38 +0800

    add sample about workspace

---
+++ rust-programming-languange/ch14/add/add-one/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn fun_add_one(x: i32) -> i32 {
+    return x + 1;
+}
---
+++ rust-programming-languange/ch14/add/adder/src/main.rs
@@ -0,0 +1,6 @@
+use add_one;
+
+fn main() {
+    let num = 10;
+    println!("Hello, world! {} plus one is {}!", num, add_one::fun_add_one(num));
+}
