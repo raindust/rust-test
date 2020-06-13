9bd5dc5b63dfed4aadb06cd338d652cd86b21a94
Yan Mingzhi <realraindust@gmail.com>
2020-06-13 23:17:01 +0800

    parse money v6

--- best-practice/parse-money/src/lib.rs
+++ best-practice/parse-money/src/lib.rs
@@ -1,23 +1,34 @@
 use std::num::ParseFloatError;
+use failure::Fail;
 
-#[derive(Debug)]
+#[derive(Debug, Fail, Eq, PartialEq)]
 pub enum MoneyError {
-   ParseError,
+    #[fail(display = "Invalid input: {}", _0)]
+    ParseAmount(ParseFloatError),
+
+    #[fail(display = "{}", _0)]
+    ParseFormatting(String),
 }
 
 fn parse_money(input: &str) -> Result<(f32, String), MoneyError> {
     let parts: Vec<&str> = input.split_whitespace().collect();
     if parts.len() != 2 {
-        Err(MoneyError::ParseError)
+        Err(MoneyError::ParseFormatting(input.to_string()))
     } else {
         let (amount, currency) = (parts[0], parts[1]);
         Ok((amount.parse()?, currency.to_string()))
     }
 }
 
+impl From<ParseFloatError> for MoneyError {
+    fn from(e: ParseFloatError) -> Self {
+        MoneyError::ParseAmount(e)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::parse_money;
+    use crate::{parse_money, MoneyError};
 
     #[test]
     fn parse_int_works() {
@@ -35,6 +46,10 @@ mod tests {
 
     #[test]
     fn parse_index_out_of_bounds() {
-        let (money, unit) = parse_money("140.01").unwrap();
+         let result = parse_money("140.01");
+        assert_eq!(true, result.is_err());
+        if let Some(e) = result.err() {
+            assert_eq!(MoneyError::ParseFormatting("140.01".to_string()), e);
+        }
     }
 }
