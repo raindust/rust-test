f7f6dd0ef712334919f4f03da2126b1f3a1a72bf
Yan Mingzhi <realraindust@gmail.com>
2020-06-13 22:44:26 +0800

    parse money v4

--- best-practice/parse-money/src/lib.rs
+++ best-practice/parse-money/src/lib.rs
@@ -1,13 +1,11 @@
-use std::num::ParseIntError;
+use std::num::ParseFloatError;
 
-fn parse_money(input: &str) -> Result<(i32, String), ParseIntError> {
+fn parse_money(input: &str) -> Result<(f32, String), ParseFloatError> {
     let parts: Vec<&str> = input.split_whitespace().collect();
     let maybe_amount = parts[0].parse()?;
     let currency = parts[1].to_string();
     return Ok((maybe_amount, currency));
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -16,13 +14,14 @@ mod tests {
     #[test]
     fn parse_int_works() {
         let (money, unit) = parse_money("120 Euro").unwrap();
-        assert_eq!(money, 120);
+        assert_eq!(money, 120f32);
         assert_eq!("Euro", unit);
     }
 
     #[test]
-    fn parse_float_fails() {
-        let result = parse_money("140.01 Euro");
-        assert_eq!(true, result.is_err());
+    fn parse_float_works() {
+        let (money, unit) = parse_money("140.01 Euro").unwrap();
+        assert_eq!(money, 140.01f32);
+        assert_eq!("Euro", unit);
     }
 }
