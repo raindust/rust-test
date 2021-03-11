6ba1516eb93662d221943697b74274261091eb05
Yan Mingzhi <realraindust@gmail.com>
2021-03-11 15:17:43 +0800

    add fern test

---
+++ third-party/fern-test/src/main.rs
@@ -0,0 +1,25 @@
+use log::{debug, error, info, trace, warn};
+
+fn main() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+
+    trace!("trace");
+    debug!("debug");
+    info!("info");
+    warn!("warn");
+    error!("error");
+    Ok(())
+}
